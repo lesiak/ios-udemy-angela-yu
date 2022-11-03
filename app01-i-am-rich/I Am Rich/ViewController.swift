a1341495b6ff664b4dcf0c39c727a667174b08f0
lesiak <lesiak@outlook.com>
2022-11-03 14:23:50 +0100

    [01-I-AM-RICH] Generated StoryBoard app

---
+++ app01-i-am-rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Adam Lesiak on 04/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
