5ebf6e814a032b842bf9c9a9afa322049b92d82e
lesiak <lesiak@outlook.com>
2022-11-09 11:25:10 +0100

    [02-DICEE] @IBAction to handle button events
    To add an IBAction: 
    - open a storyboard in Assistant mode
    - Ctrl + LMouseButton a Button 
    - drag to source code pane
    - for Buttons, connection type is set to Action by default

--- app02-dicee/Dicee-iOS13/ViewController.swift
+++ app02-dicee/Dicee-iOS13/ViewController.swift
@@ -21,5 +21,9 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
