f4b37dd3493bc431db4559e2327c4acde144be33
lesiak <lesiak@outlook.com>
2022-11-17 08:44:55 +0100

    [06-EGG-TIMER] Ternary expressions

--- app06-egg-timer/EggTimer/ViewController.swift
+++ app06-egg-timer/EggTimer/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle
-        print(hardness)
+        let cookingTime = hardness == "Soft"
+            ? softTime
+            : hardness == "Medium"
+                ? mediumTime
+                : hardTime
+        print(cookingTime)
     }
     
 }
