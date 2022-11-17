65a48c09c40dc9dfadbf518bac7e42e587406715
lesiak <lesiak@outlook.com>
2022-11-17 08:42:26 +0100

    [06-EGG-TIMER] Configure IBActions for UIButtons

--- app06-egg-timer/EggTimer/ViewController.swift
+++ app06-egg-timer/EggTimer/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        print(hardness)
+    }
+    
 }
