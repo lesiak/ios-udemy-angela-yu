22f1606f2bbda08aee2abd48d8b010a71c88d6e1
lesiak <lesiak@outlook.com>
2022-11-26 07:57:00 +0100

    [08-DESTINI] Extracted StoryBrain

--- app08-destini/Destini-iOS13/Controller/ViewController.swift
+++ app08-destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,12 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
-    
-    var stepNumber = 0
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,23 +22,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        let currentStep = stories[stepNumber]
-        if (sender.currentTitle == currentStep.choice1) {
-            stepNumber = 2 * stepNumber + 1
-        } else {
-            stepNumber = 2 * stepNumber + 2
-        }
-        if (stepNumber >= stories.count) {
-            stepNumber = 0
-        }
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
         updateUI()
     }
     
     func updateUI() {
-        let currentStep = stories[stepNumber]
-        storyLabel.text = currentStep.title
-        choice1Button.setTitle(currentStep.choice1, for: .normal)
-        choice2Button.setTitle(currentStep.choice2, for: .normal)
+        let currentStory = storyBrain.getCurrentStory()
+        storyLabel.text = currentStory.title
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
     }
 }
 
--- app08-destini/Destini-iOS13/Model/StoryBrain.swift
+++ app08-destini/Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,27 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    var storyNumber = 0
+    
+    mutating func nextStory(userChoice: String) {
+        let currentStory = stories[storyNumber]
+        if (userChoice == currentStory.choice1) {
+            storyNumber = 2 * storyNumber + 1
+        } else {
+            storyNumber = 2 * storyNumber + 2
+        }
+        if (storyNumber >= stories.count) {
+            storyNumber = 0
+        }
+    }
+    
+    func getCurrentStory() -> Story {
+        return stories[storyNumber]
+    }
+}
