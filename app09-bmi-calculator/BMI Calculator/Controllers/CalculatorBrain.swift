87e7327dc23058a2b331e9e23234d7a8fd55e427
lesiak <lesiak@outlook.com>
2022-12-03 22:36:54 +0100

    [09-BMI] #colorLiteral

--- app09-bmi-calculator/BMI Calculator/Controllers/CalculatorBrain.swift
+++ app09-bmi-calculator/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -7,11 +7,11 @@ struct CalculatorBrain {
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / (height * height)
         if bmiValue < 18.5 {
-            bmiResult = BmiResult(value: bmiValue, advice: "Eat more Pies", color: .blue)
+            bmiResult = BmiResult(value: bmiValue, advice: "Eat more Pies", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
         } else if bmiValue < 24.9 {
-            bmiResult = BmiResult(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+            bmiResult = BmiResult(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
         } else {
-            bmiResult = BmiResult(value: bmiValue, advice: "Eat less Pies!", color: .red)
+            bmiResult = BmiResult(value: bmiValue, advice: "Eat less Pies!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
         }
     }
     
