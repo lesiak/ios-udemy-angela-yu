09c2e3c681e9eeaf456d6145043b1f33321354ea
lesiak <lesiak@outlook.com>
2022-12-05 09:51:39 +0100

    [10-TIPSY] IBActions and IBOutlets for the Results Screen

---
+++ app10-tipsy/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+
+}
